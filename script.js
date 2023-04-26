f2b890ef0c76197f4e19b7a6bb97a99943c3d1a1
BrandonKerns <btkerns96@gmail.com>
2023-04-26 12:13:01 -0700

    Adding starting files/code

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={33033f55ad7de380f9f9c2c6d8ce431f}';
+
+const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search-input');
+const historyList = document.getElementById('history-list');
+const currentWeatherInfo = document.getElementById('current-weather-info');
+const forecastContainer = document.getElementById('forecast-container');
