573a44f50436c00c54edd3ec2e02434a2e320a61
BrandonKerns <btkerns96@gmail.com>
2023-04-26 12:41:44 -0700

    Adding weather API functions

--- script.js
+++ script.js
@@ -26,3 +26,64 @@ function addToHistory(city) {
     li.textContent = city;
     historyList.appendChild(li);
 }
+
+async function fetchWeatherData(city) {
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const [currentWeatherRes, forecastRes] = await Promise.all([
+            fetch(currentWeatherUrl),
+            fetch(forecastUrl),
+        ]);
+
+        const [currentWeatherData, forecastData] = await Promise.all([
+            currentWeatherRes.json(),
+            forecastRes.json(),
+        ]);
+
+        displayCurrentWeather(currentWeatherData);
+        displayForecast(forecastData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+function displayCurrentWeather(data) {
+    const { name, main, wind, weather } = data;
+    const date = new Date().toLocaleDateString();
+    const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+
+    currentWeatherInfo.innerHTML = `
+        <h3>${name} (${date})</h3>
+        <img src="${iconUrl}" alt="${weather[0].description}">
+        <p>Temperature: ${main.temp}°C</p>
+        <p>Humidity: ${main.humidity}%</p>
+        <p>Wind Speed: ${wind.speed} m/s</p>
+    `;
+    
+    }
+    
+    function displayForecast(data) {
+        const dailyForecasts = data.list.filter((forecast) =>
+            forecast.dt_txt.endsWith("12:00:00")
+        );
+    
+        forecastContainer.innerHTML = "";
+    
+        dailyForecasts.forEach((forecast) => {
+            const { main, weather, wind, dt_txt } = forecast;
+            const date = new Date(dt_txt).toLocaleDateString();
+            const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+    
+            const forecastDiv = document.createElement("div");
+            forecastDiv.innerHTML = `
+                <h4>${date}</h4>
+                <img src="${iconUrl}" alt="${weather[0].description}">
+                <p>Temperature: ${main.temp}°C</p>
+                <p>Humidity: ${main.humidity}%</p>
+                <p>Wind Speed: ${wind.speed} m/s</p>
+            `;
+            forecastContainer.appendChild(forecastDiv);
+        });
+    }
